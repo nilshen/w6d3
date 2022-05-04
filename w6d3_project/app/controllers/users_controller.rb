ee3c10a4471f4a147c63e8942bb9fe91b068cffe
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-04 15:50:50 -0400

    up to nesting

---
+++ w6d3_project/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    def index
+        render plain: params
+    end
+
+    def show
+        render json: params
+    end
+
+    def create
+        render json: params
+    end
+end 
--- w6d3_project/config/routes.rb
+++ w6d3_project/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index' #as: 'users'      #manually creating a single route
-	get '/users/:id', to: 'users#show', as: 'user'   #':id' is a wildcard. Post specific pictures
-	post '/users', to: 'users#create'
-	patch '/users/:id', to:'users#update'
-	put '/users/:id', to: 'users#update'
-	delete '/users/:id', to: 'users#destroy'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/edit', to: 'users#edit', as: 'edit_user'
+  # get '/users', to: 'users#index', as: 'banana'    #manually creating a single route
+	# get '/users/:id', to: 'users#show', as: 'banana'   #':id' is a wildcard. Post specific pictures
+	# post '/users', to: 'users#create'
+	# patch '/users/:id', to:'users#update'
+	# put '/users/:id', to: 'users#update'
+	# delete '/users/:id', to: 'users#destroy'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # get '/users/edit', to: 'users#edit', as: 'edit_user'
   
 # users get    /users(.:format)                 users#index
 # post   /users(.:format)                       users#create
@@ -18,9 +18,6 @@ Rails.application.routes.draw do
 # put    /users/:id(.:format)                   users#update
 # delete /users/:id(.:format)                   users#destroy
   
-  # resources :users
-
-
-
+  resources :users
 
 end
